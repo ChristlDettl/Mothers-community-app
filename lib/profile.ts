425dd4157eee1e377931e9f459b0ef759c914ea0
ChristlDettl <christianedettelbacher@gmx.de>
2025-09-10 16:12:16 +0200

    Update profile.ts

--- lib/profile.ts
+++ lib/profile.ts
@@ -1,6 +1,6 @@
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "./supabaseClient";
 
-export async function ensureUserProfile(user) {
+export async function ensureUserProfile(user: any) {
   const { data, error } = await supabase
     .from("profiles")
     .select("*")
@@ -12,9 +12,14 @@ export async function ensureUserProfile(user) {
       {
         id: user.id,
         email: user.email,
+        full_name: "",
+        birth_date: null,
+        num_children: 0,
+        children_ages: "[]",
+        city: "",
       },
     ]);
-
     if (insertError) console.error("Fehler beim Anlegen des Profils:", insertError);
   }
 }
+
