39c8d289484299558186ef865cdd84f44998bb34
ChristlDettl <christianedettelbacher@gmx.de>
2025-09-17 18:30:51 +0200

    Update register.tsx

--- pages/register.tsx
+++ pages/register.tsx
@@ -49,7 +49,6 @@ export default function Register() {
             .from("profiles")
             .insert([
               {
-                id: data.user.id,
                 email: data.user.email,
               },
             ]);
